35f4b0610bbcfefb7b77366419d775c601155afc
Trinh Hoai Thanh <91533983+trinhhoaithanh@users.noreply.github.com>
2023-08-17 23:18:04 +0700

    exercise 6

--- index.ts
+++ index.ts
@@ -2,22 +2,28 @@
 
 Intro:
 
-    Time to filter the data! In order to be flexible
-    we filter users using a number of criteria and
-    return only those matching all of the criteria.
-    We don't need Admins yet, we only filter Users.
+    Filtering requirements have grown. We need to be
+    able to filter any kind of Persons.
 
 Exercise:
 
-    Without duplicating type structures, modify
-    filterUsers function definition so that we can
-    pass only those criteria which are needed,
-    and not the whole User information as it is
-    required now according to typing.
+    Fix typing for the filterPersons so that it can filter users
+    and return User[] when personType='user' and return Admin[]
+    when personType='admin'. Also filterPersons should accept
+    partial User/Admin type according to the personType.
+    `criteria` argument should behave according to the
+    `personType` argument value. `type` field is not allowed in
+    the `criteria` field.
 
 Higher difficulty bonus exercise:
 
-    Exclude "type" from filter criteria.
+    Implement a function `getObjectKeys()` which returns more
+    convenient result for any argument given, so that you don't
+    need to cast it.
+
+    let criteriaKeys = Object.keys(criteria) as (keyof User)[];
+    -->
+    let criteriaKeys = getObjectKeys(criteria);
 
 */
 
@@ -39,70 +45,44 @@ export type Person = User | Admin;
 
 export const persons: Person[] = [
     { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
-    {
-        type: 'admin',
-        name: 'Jane Doe',
-        age: 32,
-        role: 'Administrator'
-    },
-    {
-        type: 'user',
-        name: 'Kate Müller',
-        age: 23,
-        occupation: 'Astronaut'
-    },
-    {
-        type: 'admin',
-        name: 'Bruce Willis',
-        age: 64,
-        role: 'World saver'
-    },
-    {
-        type: 'user',
-        name: 'Wilson',
-        age: 23,
-        occupation: 'Ball'
-    },
-    {
-        type: 'admin',
-        name: 'Agent Smith',
-        age: 23,
-        role: 'Administrator'
-    }
+    { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
+    { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
+    { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' },
+    { type: 'user', name: 'Wilson', age: 23, occupation: 'Ball' },
+    { type: 'admin', name: 'Agent Smith', age: 23, role: 'Anti-virus engineer' }
 ];
 
-export const isAdmin = (person: Person): person is Admin => person.type === 'admin';
-export const isUser = (person: Person): person is User => person.type === 'user';
-
 export function logPerson(person: Person) {
-    let additionalInformation = '';
-    if (isAdmin(person)) {
-        additionalInformation = person.role;
-    }
-    if (isUser(person)) {
-        additionalInformation = person.occupation;
-    }
-    console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
+    console.log(
+        ` - ${person.name}, ${person.age}, ${person.type === 'admin' ? person.role : person.occupation}`
+    );
 }
 
-export function filterUsers(persons: Person[], criteria: Partial<Omit<User,'type'>>): User[] {
-    return persons.filter(isUser).filter((user) => {
-        const criteriaKeys = Object.keys(criteria) as (keyof Omit<User,'type'>)[];
-        return criteriaKeys.every((fieldName) => {
-            return user[fieldName] === criteria[fieldName];
+const getObjectKeys = <T>(obj:T) => Object.keys(obj) as (keyof T)[];
+
+export function filterPersons(persons: Person[], personType: User['type'], criteria: Partial<Omit<User,'type'>>): User[];
+export function filterPersons(persons: Person[], personType: Admin['type'], criteria: Partial<Omit<Admin,'type'>>): Admin[];
+export function filterPersons(persons: Person[], personType: Person['type'], criteria: Partial<Person>): Person[]{
+    return persons
+        .filter((person) => person.type === personType)
+        .filter((person) => {
+            let criteriaKeys = getObjectKeys(criteria);
+            return criteriaKeys.every((fieldName) => {
+                return person[fieldName] === criteria[fieldName];
+            });
         });
-    });
 }
+
+export const usersOfAge23 = filterPersons(persons, 'user', { age: 23 });
+export const adminsOfAge23 = filterPersons(persons, 'admin', { age: 23 });
 
 console.log('Users of age 23:');
+usersOfAge23.forEach(logPerson);
 
-filterUsers(
-    persons,
-    {
-        age: 23
-    }
-).forEach(logPerson);
+console.log();
+
+console.log('Admins of age 23:');
+adminsOfAge23.forEach(logPerson);
 
 // In case you are stuck:
-// https://www.typescriptlang.org/docs/handbook/utility-types.html
-// https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-8.html#predefined-conditional-types
+// https://www.typescriptlang.org/docs/handbook/2/functions.html#function-overloads
