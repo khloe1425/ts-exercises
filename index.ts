9e6e5ca37e090b6010330dad1f94d53157018f53
Trinh Hoai Thanh <91533983+trinhhoaithanh@users.noreply.github.com>
2023-08-20 15:26:35 +0700

    exercise 12

--- index.ts
+++ index.ts
@@ -1,32 +1,49 @@
 import {
-    strReverse,
-    strToLower,
-    strToUpper,
-    strRandomize,
-    strInvertCase
-} from 'str-utils';
+    getMaxIndex,
+    getMaxElement,
+    getMinIndex,
+    getMinElement,
+    getMedianIndex,
+    getMedianElement,
+    getAverageValue
+} from 'stats';
 
 /*
 
 Intro:
 
-    In order to engage users in the communication with
-    each other we have decided to decorate usernames
-    in various ways. A brief search led us to a library
-    called "str-utils". Bad thing is that it lacks
-    TypeScript declarations.
+    We have so many users and admins in the database!
+    CEO's father Jeff says that we are a BigData
+    startup now. We have no idea what it means, but
+    Jeff says that we need to do some statistics and
+    analytics.
+
+    We've ran a questionnaire within the team to
+    figure out what do we know about statistics.
+    The only person who filled it was our coffee
+    machine maintainer. The answers were:
+
+     * Maximums
+     * Minumums
+     * Medians
+     * Averages
+
+    We found a piece of code on stackoverflow and
+    compiled it into a module `stats`. The bad
+    thing is that it lacks type declarations.
 
 Exercise:
 
-    Check str-utils module implementation at:
-    node_modules/str-utils/index.js
-    node_modules/str-utils/README.md
+    Check stats module implementation at:
+    node_modules/stats/index.js
+    node_modules/stats/README.md
 
     Provide type declaration for that module in:
-    declarations/str-utils/index.d.ts
+    declarations/stats/index.d.ts
 
-    Try to avoid duplicates of type declarations,
-    use type aliases.
+Higher difficulty bonus exercise:
+
+    Avoid duplicates of type declarations.
 
 */
 
@@ -44,8 +61,6 @@ interface Admin {
     role: string;
 }
 
-type Person = User | Admin;
-
 const admins: Admin[] = [
     { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
     { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' },
@@ -62,37 +77,78 @@ const users: User[] = [
     { type: 'user', name: 'Inspector Gadget', age: 31, occupation: 'Undercover' }
 ];
 
-const isAdmin = (person: Person): person is Admin => person.type === 'admin';
-const isUser = (person: Person): person is User => person.type === 'user';
-
-export const nameDecorators = [
-    strReverse,
-    strToLower,
-    strToUpper,
-    strRandomize,
-    strInvertCase
-];
-
-function logPerson(person: Person) {
-    let additionalInformation: string = '';
-    if (isAdmin(person)) {
-        additionalInformation = person.role;
+function logUser(user: User | null) {
+    if (!user) {
+        console.log(' - none');
+        return;
     }
-    if (isUser(person)) {
-        additionalInformation = person.occupation;
-    }
-    const randomNameDecorator = nameDecorators[
-        Math.round(Math.random() * (nameDecorators.length - 1))
-    ];
-    const name = randomNameDecorator(person.name);
-    console.log(
-        ` - ${name}, ${person.age}, ${additionalInformation}`
-    );
+    const pos = users.indexOf(user) + 1;
+    console.log(` - #${pos} User: ${user.name}, ${user.age}, ${user.occupation}`);
 }
 
-([] as Person[])
-    .concat(users, admins)
-    .forEach(logPerson);
+function logAdmin(admin: Admin | null) {
+    if (!admin) {
+        console.log(' - none');
+        return;
+    }
+    const pos = admins.indexOf(admin) + 1;
+    console.log(` - #${pos} Admin: ${admin.name}, ${admin.age}, ${admin.role}`);
+}
 
-// In case you are stuck:
-// https://www.typescriptlang.org/docs/handbook/modules.html#ambient-modules
+const compareUsers = (a: User, b: User) => a.age - b.age;
+const compareAdmins = (a: Admin, b: Admin) => a.age - b.age;
+const colorizeIndex = (value: number) => String(value + 1);
+
+export {
+    getMaxIndex,
+    getMaxElement,
+    getMinIndex,
+    getMinElement,
+    getMedianIndex,
+    getMedianElement,
+    getAverageValue
+};
+
+console.log('Youngest user:');
+logUser(getMinElement(users, compareUsers));
+console.log(` - was ${colorizeIndex(getMinIndex(users, compareUsers))}th to register`);
+
+console.log();
+
+console.log('Median user:');
+logUser(getMedianElement(users, compareUsers));
+console.log(` - was ${colorizeIndex(getMedianIndex(users, compareUsers))}th to register`);
+
+console.log();
+
+console.log('Oldest user:');
+logUser(getMaxElement(users, compareUsers));
+console.log(` - was ${colorizeIndex(getMaxIndex(users, compareUsers))}th to register`);
+
+console.log();
+
+console.log('Average user age:');
+console.log(` - ${String(getAverageValue(users, ({age}: User) => age))} years`);
+
+console.log();
+
+console.log('Youngest admin:');
+logAdmin(getMinElement(admins, compareAdmins));
+console.log(` - was ${colorizeIndex(getMinIndex(users, compareUsers))}th to register`);
+
+console.log();
+
+console.log('Median admin:');
+logAdmin(getMedianElement(admins, compareAdmins));
+console.log(` - was ${colorizeIndex(getMedianIndex(users, compareUsers))}th to register`);
+
+console.log();
+
+console.log('Oldest admin:');
+logAdmin(getMaxElement(admins, compareAdmins));
+console.log(` - was ${colorizeIndex(getMaxIndex(users, compareUsers))}th to register`);
+
+console.log();
+
+console.log('Average admin age:');
+console.log(` - ${String(getAverageValue(admins, ({age}: Admin) => age))} years`);
